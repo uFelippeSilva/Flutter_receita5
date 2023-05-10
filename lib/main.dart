35893e2cab56d2620ff615d9cf7ce50e8575398b
uFelippeSilva <112040769+uFelippeSilva@users.noreply.github.com>
2023-05-09 23:14:11 -0300

    Gerencias de estados Feita

--- lib/main.dart
+++ lib/main.dart
@@ -1,127 +1,159 @@
 import 'package:flutter/material.dart';
-
-var dataObjects = [];
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   MyApp app = MyApp();
   runApp(app);
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    print("no build da classe MyApp");
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Aba de pesquisa"),
-        ),
-        bottomNavigationBar: NewNavBar2(onIndexChanged: (index) {
-          print("Botão tocado: $index");
-        }),
-        body: Stack(
-          children: [
-            Positioned.fill(
-              child: Image.network(
-                "https://img.freepik.com/fotos-gratis/refrigerante-colorido-bebe-tiro-macro_53876-18225.jpg",
-                fit: BoxFit.cover,
-              ),
-            ),
-            Center(
-              child: Text(
-                "Seja bem vindo a nossa Loja de Drinks, abaixo temos nossas listas de bebidas!",
-                style: TextStyle(fontSize: 32, color: Colors.white),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+class DataService {
+  final ValueNotifier<List> tableStateNotifier = new ValueNotifier([]);
+
+  var chaves = ["name", "style", "ibu"];
+  var colunas = ["Nome", "Estilo", "IBU"];
+
+  void carregar(index) {
+    var funcoes = [
+      carregarCafes,
+      carregarCervejas,
+      carregarNacoes,
+    ];
+
+    funcoes[index]();
+  }
+
+  void columnCafes() {
+    chaves = ["name", "type", "meeling"];
+    colunas = ["Nome", "Tipo", "Moagem"];
+  }
+
+  void columnCervejas() {
+    chaves = ["name", "style", "ibu"];
+    colunas = ["Nome", "Estilo", "IBU"];
+  }
+
+  void columnNacoes() {
+    chaves = ["name", "population", "pib"];
+    colunas = ["Nome", "População", "PIB"];
+  }
+
+  void carregarCafes() {
+    columnCafes();
+
+    tableStateNotifier.value = [
+      {"name": "Café do Brasil", "type": "Arábica", "meeling": "Média"},
+      {"name": "Café Colombiano", "type": "Arábica", "meeling": "Média"},
+      {"name": "Café de Minas", "type": "Arábica", "meeling": "Fina"}
+    ];
+  }
+
+  void carregarCervejas() {
+    columnCervejas();
+
+    tableStateNotifier.value = [
+      {"name": "IPA da Califórnia", "style": "India Pale Ale", "ibu": "65"},
+      {"name": "Lager Alemã", "style": "Lager", "ibu": "54"},
+      {"name": "Belgian Dubbel", "style": "Belgian Dubbel", "ibu": "82"}
+    ];
+  }
+
+  void carregarNacoes() {
+    columnNacoes();
+
+    tableStateNotifier.value = [
+      {
+        "name": "Estados Unidos",
+        "population": "331,9 milhões",
+        "pib": "22,3 trilhões"
+      },
+      {"name": "China", "population": "1,4 bilhões", "pib": "17,1 trilhões"},
+      {"name": "França", "population": "67,9 milhões", "pib": "2,9 trilhões"}
+    ];
   }
 }
 
-class NewNavBar2 extends StatefulWidget {
-  final Function(int) onIndexChanged;
+final dataService = DataService();
 
-  NewNavBar2({required this.onIndexChanged});
-
+class MyApp extends StatelessWidget {
   @override
-  _NewNavBar2State createState() => _NewNavBar2State();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Dicas"),
+          ),
+          body: ValueListenableBuilder(
+              valueListenable: dataService.tableStateNotifier,
+              builder: (_, value, __) {
+                return DataTableWidget(
+                  jsonObjects: value,
+                  propertyNames: dataService.chaves,
+                  columnNames: dataService.colunas,
+                );
+              }),
+          bottomNavigationBar:
+              NewNavBar(itemSelectedCallback: dataService.carregar),
+        ));
+  }
 }
 
-class _NewNavBar2State extends State<NewNavBar2> {
-  int _currentIndex = 0;
+class NewNavBar extends HookWidget {
+  var itemSelectedCallback;
+
+  NewNavBar({this.itemSelectedCallback}) {
+    itemSelectedCallback ??= (_) {};
+  }
 
   @override
   Widget build(BuildContext context) {
-    print("no build da classe NewNavBar2");
-
+    var state = useState(1);
     return BottomNavigationBar(
-      onTap: (index) {
-        setState(() {
-          _currentIndex = index;
-        });
-        widget.onIndexChanged(index);
-      },
-      currentIndex: _currentIndex,
-      items: const [
-        BottomNavigationBarItem(
-          label: "Cafés",
-          icon: Icon(Icons.coffee_outlined),
-        ),
-        BottomNavigationBarItem(
-          label: "Cervejas",
-          icon: Icon(Icons.local_drink_outlined),
-        ),
-        BottomNavigationBarItem(
-          label: "Sucos",
-          icon: Icon(Icons.local_bar_outlined),
-        ),
-      ],
-    );
+        onTap: (index) {
+          state.value = index;
+          itemSelectedCallback(index);
+        },
+        currentIndex: state.value,
+        items: const [
+          BottomNavigationBarItem(
+            label: "Cafés",
+            icon: Icon(Icons.coffee_outlined),
+          ),
+          BottomNavigationBarItem(
+              label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+          BottomNavigationBarItem(
+              label: "Nações", icon: Icon(Icons.flag_outlined))
+        ]);
   }
 }
 
 class DataTableWidget extends StatelessWidget {
   final List jsonObjects;
 
-  DataTableWidget({this.jsonObjects = const []});
+  final List<String> columnNames;
+
+  final List<String> propertyNames;
+
+  DataTableWidget(
+      {this.jsonObjects = const [],
+      this.columnNames = const ["Nome", "Estilo", "IBU"],
+      this.propertyNames = const ["name", "style", "ibu"]});
 
   @override
   Widget build(BuildContext context) {
-    print("no build da classe DataTableWidget");
-
-    var columnNames = ["Nome", "Estilo", "IBU"];
-    var propertyNames = ["name", "style", "ibu"];
-
     return DataTable(
-      columns: columnNames
-          .map(
-            (name) => DataColumn(
-              label: Expanded(
-                child: Text(
-                  name,
-                  style: TextStyle(fontStyle: FontStyle.italic),
-                ),
-              ),
-            ),
-          )
-          .toList(),
-      rows: jsonObjects
-          .map(
-            (obj) => DataRow(
-              cells: propertyNames
-                  .map(
-                    (propName) => DataCell(
-                      Text(obj[propName]),
-                    ),
-                  )
-                  .toList(),
-            ),
-          )
-          .toList(),
-    );
+        columns: columnNames
+            .map((name) => DataColumn(
+                label: Expanded(
+                    child: Text(name,
+                        style: TextStyle(fontStyle: FontStyle.italic)))))
+            .toList(),
+        rows: jsonObjects
+            .map((obj) => DataRow(
+                cells: propertyNames
+                    .map((propName) => DataCell(Text(obj[propName])))
+                    .toList()))
+            .toList());
   }
 }
